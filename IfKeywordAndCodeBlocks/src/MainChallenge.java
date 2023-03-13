f53479697efbff708038ab03d757f627512a4af5
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-03-12 19:43:17 -0700

    Finishes L5.46

--- IfKeywordAndCodeBlocks/src/MainChallenge.java
+++ IfKeywordAndCodeBlocks/src/MainChallenge.java
@@ -1,26 +1,17 @@
 public class MainChallenge {
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
-
-
-        calculateScore(gameOver, score, levelCompleted, bonus);
-        
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        int highScore = calculateScore(true, 800, 5, 100);
+        System.out.println("The highScore is " + highScore);
+        System.out.println("The next highScore is " +
+        calculateScore(true, 10000, 8, 200));
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        int finalScore = score;
         if (gameOver) {
-            int finalScore = score;
             finalScore += (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
         }
-
+        return finalScore;
     }
 }
