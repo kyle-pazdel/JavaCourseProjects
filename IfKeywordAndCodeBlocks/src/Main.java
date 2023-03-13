aebd8481462847298ebae48327f5bc6fb5f2ea1f
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-03-12 19:16:37 -0700

    Finishes L5.44

--- IfKeywordAndCodeBlocks/src/Main.java
+++ IfKeywordAndCodeBlocks/src/Main.java
@@ -6,6 +6,10 @@ public class Main {
         int bonus = 100;
         if (score == 5000) {
             System.out.println("Your score was 5000");
+        } else if (score < 1000) {
+            System.out.println("Your score was less than 1000");
+        } else {
+            System.out.println("Got here");
         }
     }
 }
