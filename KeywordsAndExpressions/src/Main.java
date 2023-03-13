27fa349e0e3dc709ed2a2a74c4bc37626dec66c8
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-03-12 18:40:49 -0700

    Finishes L5.41

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double mileage = 100;
+        double kilometers = (mileage * 1.609344);
+
+        int highScore = 50;
+        if (highScore > 25) {
+            highScore = 1000 + highScore; // add bonus points
+        }
+
+        int health = 1;
+        if ((health < 25) && (highScore > 1000)) {
+            highScore -= 1000;
+            System.out.println(highScore);
+        }
     }
 }
