8e1ba85cc10722fe9b4d667c0fb24331030e0b8c
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-03-16 12:25:54 -0700

    Adds additional switch example

--- SwitchStatements/src/Main.java
+++ SwitchStatements/src/Main.java
@@ -8,16 +8,19 @@ public class Main {
           } else {
             System.out.println("Was not 1 or 2");
           }
-          int switchValue = 2;
+          int switchValue = 5;
           switch (switchValue) {
               case 1:
-                      System.out.println("Switch value was 1");
-                      break;
+                  System.out.println("Switch value was 1");
+                  break;
               case 2:
                   System.out.println("Switch value was 2");
                   break;
+              case 3: case 4: case 5:
+                  System.out.println("Actually it was a " + switchValue);
+                  break;
               default:
-                  System.out.println("Was not 1 or 2");
+                  System.out.println("Was not 1, 2, 3, 4 or 5");
                   break;
           }
     }
