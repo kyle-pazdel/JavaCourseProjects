d9266d39b23239bcc437ef482b05e9299dc2699c
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-03-16 12:19:01 -0700

    Adds initial switch example

---
+++ SwitchStatements/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+          int value = 3;
+          if (value == 1) {
+              System.out.println("Value was 1");
+          } else if (value == 2) {
+            System.out.println("Value was 2");
+          } else {
+            System.out.println("Was not 1 or 2");
+          }
+          int switchValue = 2;
+          switch (switchValue) {
+              case 1:
+                      System.out.println("Switch value was 1");
+                      break;
+              case 2:
+                  System.out.println("Switch value was 2");
+                  break;
+              default:
+                  System.out.println("Was not 1 or 2");
+                  break;
+          }
+    }
+}
