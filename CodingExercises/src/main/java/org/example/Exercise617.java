56374b8571737a17b32735a6c284a8e952e68589
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-03-23 14:54:42 -0700

    Completes exercise 6.17

---
+++ CodingExercises/src/main/java/org/example/Exercise617.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Exercise617 {
+
+    public static void main(String[] args) {
+            isPalindrome(12521);
+//        System.out.println(-1221);
+//        System.out.println(707);
+//        System.out.println(11212);
+    }
+
+    public static boolean isPalindrome(int number) {
+        if (number < 0) {
+            number *= -1;
+        }
+
+        int reverse = 0;
+        int stored = number;
+        while (stored > 0) {
+            int digit = stored % 10;
+            reverse = (reverse * 10) + digit;
+            stored /= 10;
+        }
+
+        return number == reverse;
+    }
+
+}
