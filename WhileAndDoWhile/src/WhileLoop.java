bc18940eab3a3945caa1d3684922ab3fcc8244bb
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-03-23 11:24:46 -0700

    Finishes L6.64

---
+++ WhileAndDoWhile/src/WhileLoop.java
@@ -0,0 +1,35 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+
+//        for (int i = 1; i <= 5; i++) {
+//            System.out.println(i);
+//        }
+//        int j = 1;
+//        while (true) {
+//            System.out.println(j);
+//            j++;
+//            if (j > 5) {
+//                break;
+//            }
+//        }
+//        int k = 1;
+//        boolean isReady = false;
+//        do {
+//            if (k > 5) {
+//                break;
+//            }
+//            System.out.println("K: " + k);
+//            k++;
+//            isReady = (j > 0);
+//        } while (isReady);
+
+        int number = 0;
+        while (number < 50) {
+            number += 5;
+            if (number % 25 == 0){
+                continue;
+            }
+            System.out.print(number + "_");
+        }
+    }
+}
