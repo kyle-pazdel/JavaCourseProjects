ee14705df217a7696428fa245ad81cbcb5a74f45
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-03-23 12:59:53 -0700

    Finishes L6.66

--- WhileAndDoWhile/src/DigitSumChallenge.java
+++ WhileAndDoWhile/src/DigitSumChallenge.java
@@ -1,2 +1,22 @@
 public class DigitSumChallenge {
+
+    public static void main(String[] args) {
+        sumDigits(1234);
+        sumDigits(125);
+        sumDigits(1000);
+        sumDigits(1);
+    }
+
+    public static int sumDigits(int number) {
+        if (number < 0) return -1;
+        int sum = 0;
+        while (number != 0) {
+            System.out.println("Number: " + number);
+            sum += number % 10;
+            number = number / 10;
+        }
+        System.out.println("Sum: " + sum);
+        System.out.println("_____");
+        return sum;
+    }
 }
