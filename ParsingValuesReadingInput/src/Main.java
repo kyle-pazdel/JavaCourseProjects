462cffb10cd9cb236de578e14a32aa1a6de875c2
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-04-04 14:43:30 -0700

    Adds example of getting user input using System.console

--- ParsingValuesReadingInput/src/Main.java
+++ ParsingValuesReadingInput/src/Main.java
@@ -1,17 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         int currentYear = 2023;
-        String usersDateOfBirth = "1995";
 
-        int dateOfBirth = Integer.parseInt(usersDateOfBirth);
+        System.out.println(getInputFromConsole(currentYear));
 
-        System.out.println("Age = " + (currentYear - dateOfBirth));
-
-        String usersAgeWithPartialYear = "22.5";
-        double ageWithPartialYear = Double.parseDouble(usersAgeWithPartialYear);
-        System.out.println("The user says he's " + ageWithPartialYear);
+        System.out.println(getInputFromScanner(currentYear));
     }
 
+    public static String getInputFromConsole(int currentYear) {
+        String name = System.console().readLine("Hi, what's your name? ");
+        System.out.println("Hi " + name + ", Thanks for taking the course!");
+        String dateOfBirth = System.console().readLine("What year were you born? ");
+        int age = currentYear - Integer.parseInt(dateOfBirth);
 
+        return "So you are " + age + " years old";
+    }
 
+    public static String getInputFromScanner(int currentYear) {
+        return "";
+    }
 }
