b8ce2160277cb93a41dc4562c301c4e0b99048d9
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-04-04 15:44:17 -0700

    Completes exercise 6/25

---
+++ CodingExercises/src/main/java/Exercise625.java
@@ -0,0 +1,21 @@
+public class Exercise625 {
+
+    public static void main(String[] args) {
+//        System.out.println(canPack (1, 0, 4));
+//        System.out.println(canPack (1, 0, 5));
+//        System.out.println(canPack (0, 5, 4));
+//        System.out.println(canPack (2, 2, 11));
+//        System.out.println(canPack (-3, 2, 12));
+        System.out.println(canPack (2, 7, 18));
+        System.out.println(canPack(2, 10, 21));
+
+    }
+
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+        if (bigCount < 0 || smallCount < 0 || goal < 0) return false;
+
+        int bigCountToUse = bigCount < goal/5 ? bigCount : goal/5 ;
+        return (goal - bigCountToUse*5) <= smallCount;
+    }
+
+}
