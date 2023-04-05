af3ea4c0399c5c77b2ca2b803a83b26cee07ea9f
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-04-04 17:15:00 -0700

    Completes 'Reading User Input Challenge'

---
+++ ParsingValuesReadingInput/src/ReadingUserInputChallenge.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ReadingUserInputChallenge {
+
+    public static void main(String[] args) {
+        enterFiveNumbers();
+    }
+
+    public static void enterFiveNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        int number = 1;
+        double sum = 0;
+        do {
+            System.out.println("Enter number #:" + number);
+            try {
+                sum += checkData(scanner.nextLine());
+                number++;
+            } catch (NumberFormatException badUserData) {
+                System.out.println("You must enter a valid number!!!");
+            }
+        } while (number <= 5);
+        
+        System.out.println("The sum of these numbers is: " + sum);
+    }
+
+    public static double checkData(String input) {
+//        int number = Integer.parseInt(input);
+        double number = Double.parseDouble(input);
+        return number;
+    }
+}
